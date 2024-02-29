2e416f36b3a2c28867795c2ec561bd1435f0b5a6
Pauline <git@ethanlibs.co>
2024-02-28 23:14:23 -0500

    bump mods and add chunk loader (#43)
    * bump enchant, collective, and other optimization mods
    
    remove better mount hud since it was causing issues
    
    * add create: chunk loader mod no config yet dont try yet pls
    
    * bump treeharvester and balm
    
    * format files
    
    * add some default configs so we can mess around with them
    
    * start kubejs power loader recipe changes
    
    * add kubejs tiered recipe

---
+++ kubejs/server_scripts/power_loader.js
@@ -0,0 +1,39 @@
+ServerEvents.recipes((event) => {
+	event.remove({ output: 'create_power_loader:empty_brass_chunk_loader' });
+
+	event.shapeless('create_power_loader:empty_andesite_chunk_loader', [
+		'create_power_loader:empty_brass_chunk_loader',
+	]);
+
+	event.custom({
+		type: 'create:mechanical_crafting',
+
+		pattern: [
+			'GGGGG',
+			'G   G',
+			'G R G',
+			'CPPPC',
+			'CCSCC',
+		],
+
+		key: {
+			G: {
+				tag: 'c:glass_blocks',
+			},
+			R: {
+				item: 'create_power_loader:empty_andesite_chunk_loader',
+			},
+			S: {
+				item: 'create:shaft',
+			},
+			C: {
+				item: 'create:brass_casing',
+			},
+			P: {
+				item: 'create:precision_mechanism',
+			},
+		},
+
+		result: { item: 'create_power_loader:empty_brass_chunk_loader', count: 1 },
+	});
+});
