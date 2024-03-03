921d52fd9bf60c8502b016f42b8ada3852fe2164
Pauline <git@ethanlibs.co>
2024-03-03 16:17:40 -0500

    (balancing): make hyper exp a little less difficult to collect (#45)
    * (balancing): make hyper exp a little less difficult to collect
    
    * remove silly ink sac recipe
    
    * fmt and refresh

---
+++ kubejs/server_scripts/create_hyper_exp.js
@@ -0,0 +1,29 @@
+ServerEvents.recipes((event) => {
+	event.remove({ output: 'create_enchantment_industry:hyper_experience' });
+
+	event.custom({
+		type: 'create:mixing',
+
+		ingredients: [
+			{
+				item: 'minecraft:glow_ink_sac',
+			},
+			{
+				item: 'minecraft:lapis_lazuli',
+			},
+			{
+				fluid: 'create_enchantment_industry:experience',
+				amount: 8100,
+			},
+		],
+
+		results: [
+			{
+				fluid: 'create_enchantment_industry:hyper_experience',
+				amount: 4050,
+			},
+		],
+
+		heatRequirement: 'superheated',
+	});
+});
