4e522c337990a2f22e12d253b3a16f8b8372e2db
Pauline <git@ethanlibs.co>
2024-03-09 23:31:57 +0100

    (feat): add estrogen mod

--- kubejs/server_scripts/grappling.js
+++ kubejs/server_scripts/grappling.js
@@ -1,15 +1,15 @@
-ServerEvents.recipes(event => {
-	event.remove({mod:"grapplemod",not:[{id:"grapplemod:grapplebow"}]})
+ServerEvents.recipes((event) => {
+	event.remove({ mod: 'grapplemod', not: [{ id: 'grapplemod:grapplebow' }] });
 	event.shapeless(
-		"grapplemod:longfallboots",
-		Item.of("minecraft:chainmail_boots").enchant("minecraft:feather_falling", 5).weakNBT()
+		'grapplemod:longfallboots',
+		Item.of('minecraft:chainmail_boots').enchant('minecraft:feather_falling', 5).weakNBT(),
 	).modifyResult((inventory, itemstack) => {
-		let nbt = inventory.find(Item.of("minecraft:chainmail_boots")).copy().nbt
-		let newEnchantments = nbt.Enchantments.filter(enchant => enchant.id !== "minecraft:feather_falling")
+		const nbt = inventory.find(Item.of('minecraft:chainmail_boots')).copy().nbt;
+		const newEnchantments = nbt.Enchantments.filter(enchant => enchant.id !== 'minecraft:feather_falling');
 		if (newEnchantments.length === 0)
 			delete nbt.Enchantments;
 		else
 			nbt.Enchantments = newEnchantments;
-		return itemstack.withNBT(nbt)
-	})
-})
+		return itemstack.withNBT(nbt);
+	});
+});
