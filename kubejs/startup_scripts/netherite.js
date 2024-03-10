a0c672a538c6c28ae0a88b49c0007ae8b682c5cb
Qualia <160456331+Qualia6@users.noreply.github.com>
2024-03-10 11:37:14 -0700

    add silly effect

--- kubejs/startup_scripts/netherite.js
+++ kubejs/startup_scripts/netherite.js
@@ -3,21 +3,35 @@ StartupEvents.registry('block', event => {
 	event.create("toxic").soundType("ancient_debris").unbreakable().model("kubejs:block/toxic")
 	event.create("pure_math").lightLevel(1).soundType("mud").hardness(0).tag("minecraft:minable/hoe")
 })
+
+StartupEvents.registry('mob_effect', event => {
+	event.create("silly").beneficial().color(0xdb481c).effectTick((entity, lvl) => global.sillyEffect(entity, lvl))
+})
+
+global.sillyEffect = (entity, lvl) => {
+	if (Math.random() < (lvl+1)/128)
+		entity.playSound("entity.cat.ambient",1,1)
+  }
+
 StartupEvents.registry('item', event => {
-	event.create('3_rune')		.texture('kubejs:item/runes/3_rune')	.displayName("Rune o' :3")			.maxStackSize(8).fireResistant()
-	event.create('aaa_rune')	.texture('kubejs:item/runes/aaa_rune')	.displayName("Rune 'o §kBoTtOmSpEaK")	.maxStackSize(8).fireResistant()
-	event.create('crime_rune')	.texture('kubejs:item/runes/crime_rune')	.displayName("Rune o' Crime")			.maxStackSize(8).fireResistant()
-	event.create('cute_rune')	.texture('kubejs:item/runes/cute_rune')	.displayName("Rune o' Cuteness")		.maxStackSize(8).fireResistant()
-	event.create('hug_rune')	.texture('kubejs:item/runes/hug_rune')	.displayName("Rune o' Hugs")			.maxStackSize(8).fireResistant()
-	event.create('love_rune')	.texture('kubejs:item/runes/love_rune')	.displayName("Rune o' Love")			.maxStackSize(8).fireResistant()
-	event.create('lust_rune')	.texture('kubejs:item/runes/lust_rune')	.displayName("Rune o' Lust")			.maxStackSize(8).fireResistant()
-	event.create('magic_rune')	.texture('kubejs:item/runes/magic_rune')	.displayName("Rune o' Magic")			.maxStackSize(8).fireResistant()
-	event.create('math_rune')	.texture('kubejs:item/runes/math_rune')	.displayName("Rune o' Mathematics")		.maxStackSize(8).fireResistant()
-	event.create('meow_rune')	.texture('kubejs:item/runes/meow_rune')	.displayName("Rune o' MEOW")			.maxStackSize(8).fireResistant()
-	event.create('power_rune')	.texture('kubejs:item/runes/power_rune')	.displayName("Rune o' INFINITE POWER")	.maxStackSize(8).fireResistant()
-	event.create('pretty_rune')	.texture('kubejs:item/runes/pretty_rune')	.displayName("Rune o' Prettiness")		.maxStackSize(8).fireResistant()
-	event.create('gay_rune')	.texture('kubejs:item/runes/gay_rune')	.displayName("Rune o' Gay")			.maxStackSize(8).fireResistant()
-	event.create('silly_rune')	.texture('kubejs:item/runes/silly_rune')	.displayName("Rune o' Sillies")		.maxStackSize(8).fireResistant()
-	event.create('smart_rune')	.texture('kubejs:item/runes/smart_rune')	.displayName("Rune o' Smartness")		.maxStackSize(8).fireResistant()
-	event.create('sleepy_rune')	.texture('kubejs:item/runes/sleepy_rune')	.displayName("Rune o' Sleepy")		.maxStackSize(8).fireResistant()
+	const addRune = (id,name) =>event.create(`${id}_rune`).texture(`kubejs:item/runes/${id}_rune`).displayName(`Rune o' ${name}`).maxStackSize(8).fireResistant().food(food=>food.alwaysEdible().hunger(2).saturation(1).eaten(ctx => {//there is a better way but kubejs registers items before foods so the better way does not work :/
+		Utils.server.runCommandSilent(`effect give ${ctx.player.uuid} kubejs:silly 30 3`)
+	}))
+
+	addRune("3",":3")
+	addRune("aaa","§kBoTtOmSpEaK")
+	addRune("crime","Crime")
+	addRune("cute","Cuteness")
+	addRune("hug","hugs")
+	addRune("love","Love")
+	addRune("lust","Lust")
+	addRune("magic","Magic")
+	addRune("math","Mathematics")
+	addRune("meow","MEOW")
+	addRune("power","INFINITE POWER")
+	addRune("pretty","Prettiness")
+	addRune("gay","Gay")
+	addRune("silly","Sillies")
+	addRune("smart","Smartness")
+	addRune("sleepy","Sleepy")
 })
